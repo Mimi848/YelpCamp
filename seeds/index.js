56fa3cacc51ccc92f7be3caf8f4a2ab6bbfbbe7f
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2020-12-28 16:10:40 +0500

    changes to the seeds file for heroku issues

--- seeds/index.js
+++ seeds/index.js
@@ -3,13 +3,18 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
+
+
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -24,8 +29,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            //YOUR USER ID
-            author: '5fdbc52c0876e469aff1ff66',
+            author: '5fe8ac3d70dd89f724442eeb',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
